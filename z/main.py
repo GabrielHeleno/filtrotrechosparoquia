f1cba8291662b8e8d32471dae475c1bd3a58ff35
GabrielHeleno <38138898+GabrielHeleno@users.noreply.github.com>
2026-01-21 19:51:27 -0300

    Delete z directory

--- z/main.py
+++
@@ -1,9 +0,0 @@
-uvicorn.run(app, host="0.0.0.0", port=8000)
-from fastapi.middleware.cors import CORSMiddleware
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
